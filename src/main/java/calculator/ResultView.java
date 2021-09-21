887db192321449d306e8c5ab80ef17ec007d6cde
gw-study <84048349+gw-study@users.noreply.github.com>
2021-09-22 08:08:51 +0900

    Revert "Step1 java-lotto 문자열 덧셈계산기 리뷰 요청드립니다."

--- src/main/java/calculator/ResultView.java
+++
@@ -1,7 +0,0 @@
-package calculator;
-
-public class ResultView {
-    public static void resultView(StringPlusCalculator stringPlusCalculator, String expression) {
-        System.out.println(stringPlusCalculator.splitAndSum(expression));
-    }
-}
